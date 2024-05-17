adbafd60ba17e5a42bb26b36b77f5c2b442fb832
ismoil <ismoiljoraxonov1@gmail.com>
2024-05-17 22:02:26 +0900

    second  commit

--- src/main/java/com/example/gitstudy/GitStudyApplication.java
+++ src/main/java/com/example/gitstudy/GitStudyApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitStudyApplication {
 
     public static void main(String[] args) {
+
+        System.out.println("");
         SpringApplication.run(GitStudyApplication.class, args);
     }
 
