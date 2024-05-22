305babe6925cd31fed9c83a8c6d167e81082d6e6
ismoil <ismoiljoraxonov1@gmail.com>
2024-05-22 22:43:45 +0900

    added my freind name and  main function

--- src/main/java/com/example/gitstudy/Test1.java
+++ src/main/java/com/example/gitstudy/Test1.java
@@ -1,4 +1,11 @@
 package com.example.gitstudy;
 
 public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("Asadbek");
+        System.out.println("Asadbek");
+        System.out.println("Asadbek");
+        System.out.println("Asadbek");
+    }
 }
