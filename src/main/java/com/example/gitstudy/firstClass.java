ff35c9e62ce4ad0c69c509cb67c516e17b126d7e
ismoil <ismoiljoraxonov1@gmail.com>
2024-05-22 23:17:41 +0900

    added A B C classes

--- src/main/java/com/example/gitstudy/firstClass.java
+++ src/main/java/com/example/gitstudy/firstClass.java
@@ -1,4 +1,11 @@
 package com.example.gitstudy;
 
+import ch.qos.logback.core.encoder.JsonEscapeUtil;
+
 public class firstClass {
+
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
 }
