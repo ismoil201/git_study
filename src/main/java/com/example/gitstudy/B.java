c71efc893aec09ee24dcf139383ab45ae6c0450a
ismoil <ismoiljoraxonov1@gmail.com>
2024-05-23 10:51:34 +0900

    BBB

--- src/main/java/com/example/gitstudy/B.java
+++ src/main/java/com/example/gitstudy/B.java
@@ -1,4 +1,11 @@
 package com.example.gitstudy;
 
 public class B {
+
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
 }
